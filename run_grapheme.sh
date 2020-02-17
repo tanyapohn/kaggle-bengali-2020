bc350352b7874b47ee933c321046f8eeaf9de431
Thunyabhorn Pathummasut <thunyabhorn.p@kbtg.tech>
2020-02-17 14:26:57 +0700

    Add device

--- run_grapheme.sh
+++ run_grapheme.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-source "/home/mod/.local/share/virtualenvs/Workspace-lCUZRoor/bin/activate"
+source "/home/mod961094/.local/share/virtualenvs/Workspace-n_bFWxY_/bin/activate"
 
-python -m bengali.grapheme.debug \
-      --test-size 128 \
-      --base 'resnet50' \
-      --batch-size 64 \
-      --fold 0 \
-      --lr 11e-3 \
-      --epochs 30 \
-      --output-dir '/home/mod/Workspace/debug/'
+for fold in 0 1 2 3;
+do
+    echo "fold ${fold}"
+    python -m bengali.grapheme.main \
+          --test-size 224 \
+          --base 'resnext50_32x4d' \
+          --batch-size 64 \
+          --fold ${fold} \
+          --lr 20e-3 \
+          --epochs 90 \
+          --device 1 \
+          --output-dir '/home/mod961094/Workspace/bengali-experiments/'
+done
